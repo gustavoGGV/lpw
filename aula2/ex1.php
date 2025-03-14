ceecb35237589c9b22417f22338f78f78004f0d4
Gustavo <134728517+gustavoGGV@users.noreply.github.com>
2025-03-14 15:39:11 -0300

    Create ex1.php

---
+++ aula2/ex1.php
@@ -0,0 +1,7 @@
+<?php
+
+$colors = array(1 => 'yellow', 2 => 'blue', 3 => 'black', 4 => 'green', 5 => 'red', 6 => 'purple', 7 => 'gray', 8 => 'brown');
+
+for ($i = 1; $i <= 8; $i++) {
+    echo "<div style='background-color: " . $colors[$i] . "; width: 100px; height: 100px;'></div>";
+};

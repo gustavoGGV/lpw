55d1f778d9ae4678f15cd029d6efd532c1c3997d
Gustavo <134728517+gustavoGGV@users.noreply.github.com>
2025-04-25 14:42:42 -0300

    Create index.php

---
+++ tarefa1/index.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "<head><link rel='stylesheet' href='styles.css'></head>";
+
+echo "<h1>Adivinhe o piloto</h1>";
+echo "<h2>Eu escolhi um piloto... tente adivinhar qual deles é o meu escolhido:</h2><br><br>";
+
+echo "<a href='palpite.php?palpite=1'>Lewis Hamilton</a><br>";
+echo "<a href='palpite.php?palpite=2'>Michael Schumacher</a><br>";
+echo "<a href='palpite.php?palpite=3'>Juan Manuel Fangio</a><br>";
+echo "<a href='palpite.php?palpite=4'>Max Verstappen</a><br>";
+echo "<a href='palpite.php?palpite=5'>Ayrton Senna</a><br>";
+echo "<a href='palpite.php?palpite=6'>Pastor Maldonado</a><br>";
